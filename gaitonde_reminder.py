f9986b3b3c4c961f90586b42b661b1ab64a1e2d9
tamamushi-iro <57350442+tamamushi-iro@users.noreply.github.com>
2025-04-01 21:07:11 -0400

    feedback changes

--- gaitonde_reminder.py
+++ gaitonde_reminder.py
@@ -1,18 +1,23 @@
 import os
 import re
+import json
 import logging
 import sqlite3 as sql
 
+from time import time
+from pprint import pformat
 from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
-from discord.ext import commands
-from datetime import datetime
+from discord.ext import tasks, commands
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 TCS_GLD_ID = os.getenv('TCS_GLD_ID')
 TCS_GENERAL_CHNL_ID = os.getenv('TCS_GENERAL_CHNL_ID')
+TCS_EVERINYAN_ROLE_ID = os.getenv('TCS_EVERINYAN_ROLE_ID')
 
 class Reminder(commands.Cog):
     def __init__(self, bot):
@@ -29,7 +34,7 @@ class Reminder(commands.Cog):
             'UTC': 'UTC'                    # Coordinated Universal Time
         }
         # self.reminder_loop.start()
-    
+
     def parse_timestamp(self, tsstr):
         m = re.match(r'(\d{1,2})[-_/](\d{1,2})[-_/](\d{2,4})[\s_T](\d{1,2}):*(\d{1,2})*\s*(am|AM|pm|PM)*\s*([a-zA-Z]{3})*', tsstr.strip())
         if m:
@@ -41,17 +46,17 @@ class Reminder(commands.Cog):
 
             timestamp_str = f'{year + 2000 if year < 100 else year}-{month:02}-{date:02} {hour:02}:{minute:02} {time_of_day}'
             dt = datetime.strptime(timestamp_str, '%Y-%m-%d %I:%M %p')
-            
+
             timezone = self.tz_map[timezone_abbr]
             dt = dt.replace(tzinfo=ZoneInfo(timezone))
             return dt
         else:
             logger.warning(f'parse_timestamp returned None for string: {tsstr}')
             return None
-    
+
     # def cog_unload(self):
     # 	self.reminder_loop.cancel()
-    
+
     @commands.command(aliases=['reminder', 'remindat'])
     async def remind(self, ctx, *, query):
         """
@@ -91,7 +96,7 @@ class Reminder(commands.Cog):
             await ctx.send(
                 f'Usage while replying to a message (<>* are optional): ``{ctx.prefix}{ctx.command} DD-MM-YYYY HH:<MM>* <AM/PM>* <IST>*``'
             )
-    
+
     @commands.command(aliases=['listreminders', 'lr'])
     async def listReminders(self, ctx):
         """Show DB Entries"""
@@ -103,3 +108,64 @@ class Reminder(commands.Cog):
             response += f"\n{i+1}. {row[1]} - {row[2]}"
         await ctx.send(f'Reminders for this Guild:\n```{response}```' if response else f'No Reminder found for this Guild.')
         conn.close()
+
+    @commands.command(aliases=["showremider", "sr"])
+    async def showReminder(self, ctx, *, query):
+        """Show Boii Info"""
+        discordID = re.match(r"^<@(\d*)>$", query.strip())
+        if discordID:
+            discordID = discordID.groups()[0]
+            conn = sql.connect(self.db_name)
+            cur = conn.cursor()
+            res = cur.execute(
+                f"SELECT * FROM {self.db_table} WHERE discordID = {discordID} and guildID = {ctx.guild.id} ORDER BY ?????;"
+            ).fetchone()
+            response = pformat(
+                {
+                    "name": res[1],
+                    "nickname": res[2],
+                    # 'discordID': res[3],
+                    # 'guild': res[7],
+                    "dob": res[4],
+                    "addedBy": res[5].split("<:>")[0],
+                    "addedOn": res[6],
+                },
+                sort_dicts=False,
+            )
+            for char in ["{", "}", "'", ",", '"']:
+                response = response.replace(char, "")
+            await ctx.send(f"Showing Boii: <@{res[3]}>\n``` {response}```")
+            conn.close()
+        else:
+            await ctx.send(f"Usage: \n``{ctx.prefix}{ctx.command} @<bdboi-mention>``")
+
+    @commands.command(aliases=[""])
+    async def nextReminder(self, ctx):
+        """Show the upcoming birthdays in the Guild for the next 3 months."""
+        today = (datetime.now() + timedelta(hours=5, minutes=30)).date()  # in IST
+        conn = sql.connect(self.db_name)
+        cur = conn.cursor()
+        res = cur.execute(
+            f"SELECT * FROM {self.db_table} WHERE guildID = {ctx.guild.id};"
+        ).fetchall()
+        conn.close()
+        next_bdays = {}
+        for row in res:
+            dob = date.fromisoformat(row[4])
+            dob_this_year = date(year=today.year, month=dob.month, day=dob.day)
+            dob_next_year = date(year=today.year + 1, month=dob.month, day=dob.day)
+            if today < dob_this_year < today + relativedelta(months=3):
+                next_bdays[dob_this_year.isoformat()] = row[2]
+            if today < dob_next_year < today + relativedelta(months=3):
+                next_bdays[dob_next_year.isoformat()] = row[2]
+        next_bdays = pformat(next_bdays, sort_dicts=True)
+        for char in ["{", "}", "'", ",", '"']:
+            next_bdays = next_bdays.replace(char, "")
+        await ctx.send(f"Upcoming BDays (next 3 months):\n``` {next_bdays}```")
+
+    # we need to wait for the bot to be ready
+    # as failing to do so will raise an attribute error:
+    # AttributeError: 'NoneType' object has no attribute 'change_presence'
+    @reminder_loop.before_loop
+    async def before_reminder_loop(self):
+        await self.bot.wait_until_ready()
